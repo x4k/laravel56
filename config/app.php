6fcacc8fbd58e0c4160bf813f6e691ef95f0453d
xufukun 00445990 <xufukun@huawei.com>
2018-06-11 18:48:05 +0800

    更新部分内容

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        #代码提示
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
